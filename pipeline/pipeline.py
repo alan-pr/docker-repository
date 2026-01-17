ca8eec8d1cb1bd5f1f174af53d9efd5a76c6fc37
alan-pr <65430572+alan-pr@users.noreply.github.com>
2026-01-17 04:23:23 +0000

    pipeline

---
+++ pipeline/pipeline.py
@@ -0,0 +1,11 @@
+import sys
+import pandas as pd
+
+month = sys.argv[1]
+
+df = pd.DataFrame({"A": [1, 2], "B": [3, 4]})
+df['month'] = month
+
+print(df.head())
+
+df.to_parquet(f"output_day_{sys.argv[1]}.parquet")
