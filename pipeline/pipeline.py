24f943841d145f75e1d3f411bf8079c3b733cd2f
alan-pr <65430572+alan-pr@users.noreply.github.com>
2026-01-18 20:54:46 +0000

    Ignore Postgres data dir

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 month = sys.argv[1]
 
-df = pd.DataFrame({"A": [1, 2], "B": [3, 4]})
+df = pd.DataFrame({"day": [1, 2], "num_passengers": [3, 4]})
 df['month'] = month
 
 print(df.head())
